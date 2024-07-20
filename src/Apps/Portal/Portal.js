b6d4dc5bd066c3a1b4cacae45c7911440ffd5238
Me6111 <maksymmpawlowski@gmail.com>
2024-07-20 02:39:13 +0100

    Initial commit

---
+++ src/Apps/Portal/Portal.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+// C:\Users\user\Desktop\projects\project-manager\src\Apps\Portal\Portal.js
+
+import React from 'react';
+import MainMenu from '../../components/MainMenu/MainMenu'; 
+import './Portal.css'; 
+import img0 from './img00.jpg'; 
+import img1 from './img1.jpg'; 
+
+
+const Portal = () => {
+    return (
+        <div className="appContainer">
+            <div className="section">
+                    <div className="imgField" id="imgField0">
+                        <img src={img0} alt="img0" />
+                        <div className="deathTitle">DEATH</div>
+                    </div>
+
+                    <div className="imgField" id="imgField00">
+                        <img src={img0} alt="img00" />
+                    </div>
+
+                    <div className="titleField">
+                        The quest to the dephths of the soul
+                    </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Portal;
+
+
---
+++ src/components/MainMenu/MainMenu.js
@@ -0,0 +1,20 @@
+
+
+
+
+// src/components/MainMenu/MainMenu.js
+
+import React from 'react';
+import './MainMenu.css'; 
+
+const MainMenu = () => {
+    return (
+        <div className="MainMenu">
+            <div className="MainMenuOption">Home</div>
+            <div className="MainMenuOption">About</div>
+            <div className="MainMenuOption">Contact</div>
+        </div>
+    );
+};
+
+export default MainMenu;
