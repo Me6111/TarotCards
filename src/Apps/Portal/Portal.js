30d02a2eb98708136973db328b2d807dfc005da4
Me6111 <maksymmpawlowski@gmail.com>
2024-07-21 20:19:13 +0100

    Initial commit

--- src/Apps/Portal/Portal.js
+++ src/Apps/Portal/Portal.js
@@ -15,22 +15,34 @@ import img0 from './img00.jpg';
 
 const Portal = () => {
     return (
-        <div className="appContainer">
+        <div className="appContainer" id="titleSection">
             <div className="section">
-                    <div className="imgField" id="imgField0">
-                        <img src={img0} alt="img0" />
+                <div className="img0scontainer">
+                    
+                    <div className="imgField" id="imgField0back">
+                        <img src={img0} alt="back" />
+                        <div className="subTitle">The journey to the end of the death</div>
+                    </div>
+                    
+
+
+                    <div className="imgField" id="imgField0front">
+                        <img src={img0} alt="front" />
                         <div className="deathTitle">DEATH</div>
+
                     </div>
 
-                    <div className="imgField" id="imgField00">
-                        <img src={img0} alt="img00" />
-                    </div>
 
-                    <div className="titleField">
-                        The quest to the dephths of the soul
-                    </div>
-
+                </div>
             </div>
+
+
+            <div className="section" id="plotSection">
+                <div className="titleField">
+                    The first metal video game
+                </div>
+            </div>
+
         </div>
     );
 };
