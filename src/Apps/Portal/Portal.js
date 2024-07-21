9ecd002f649f723ae4354d8aad2361bea16eb3ea
Me6111 <maksymmpawlowski@gmail.com>
2024-07-21 22:28:54 +0100

    Initial commit

--- src/Apps/Portal/Portal.js
+++ src/Apps/Portal/Portal.js
@@ -1,59 +1,29 @@
-
-
-
-
-
-
-// C:\Users\user\Desktop\projects\project-manager\src\Apps\Portal\Portal.js
-
 import React from 'react';
-//import MainMenu from '../../components/MainMenu/MainMenu'; 
 import './Portal.css'; 
 import img0 from './img00.jpg'; 
-//import img1 from './img1.jpg'; 
-
 
 const Portal = () => {
     return (
         <div className="appContainer" id="titleSection">
             <div className="section">
                 <div className="img0scontainer">
-                    
                     <div className="imgField" id="imgField0back">
                         <img src={img0} alt="back" />
                         <div className="subTitle">The journey to the end of the death</div>
                     </div>
-                    
-
-
                     <div className="imgField" id="imgField0front">
                         <img src={img0} alt="front" />
                         <div className="deathTitle">DEATH</div>
-
                     </div>
-
-
                 </div>
-
-
-                <div className="imgField" id="imgField0front">
-                    <img src={img0} alt="front" />
-                </div>
-
-
             </div>
-
-
             <div className="section" id="plotSection">
                 <div className="titleField">
                     The first metal video game
                 </div>
             </div>
-
         </div>
     );
 };
 
 export default Portal;
-
-
