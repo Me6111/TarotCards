4ab099f8e32f48abb7f82371e029162ef1d86ea5
Me6111 <maksymmpawlowski@gmail.com>
2024-07-22 01:24:34 +0100

    Initial commit

--- src/Apps/Portal/Portal.js
+++ src/Apps/Portal/Portal.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Portal.css'; 
 import img0 from './img00.jpg'; 
+import circleImg from './circles0.jpg';
 
 const Portal = () => {
     return (
         <div className="appContainer" id="titleSection">
             <div className="section">
                 <div className="img0scontainer">
+
                     <div className="imgField" id="imgField0back">
                         <img src={img0} alt="back" />
                         <div className="subTitle">The journey to the end of the death</div>
@@ -15,13 +17,16 @@ const Portal = () => {
                         <img src={img0} alt="front" />
                         <div className="deathTitle">DEATH</div>
                     </div>
+
                 </div>
+
+
             </div>
-            <div className="section" id="plotSection">
-                <div className="titleField">
-                    The first metal video game
-                </div>
+
+            <div className="imgField" id="circleImg">
+                <img src={circleImg} alt="circleImg" />
             </div>
+        
         </div>
     );
 };
