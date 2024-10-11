9701a251ccbcec9d335cb9662a366bc06165b5d8
Me6111 <maksymmpawlowski@gmail.com>
2024-10-11 23:08:11 +0100

    Updated app.js

--- src/Apps/Levels/LevelsSlider/LevelsSlider.js
+++ src/Apps/Levels/LevelsSlider/LevelsSlider.js
@@ -1,7 +1,3 @@
-
-
-
-
 // C:\Users\user\Desktop\projects\project-manager\src\Apps\Levels\LevelsSlider\LevelsSlider.js
 
 import React from 'react';
@@ -9,138 +5,88 @@ import './LevelsSlider.css';
 import './LevelsSlider2.css';
 import './slider-field-inner-img.css';
 
-
-import AlbumCover_1 from './albums covers/1.Scream bloody gore.jpg'; 
-import AlbumCover_2 from './albums covers/2.Leprosy.jpg'; 
-import AlbumCover_3 from './albums covers/3.Spiritual healing.jpg'; 
-import AlbumCover_4 from './albums covers/4.Human.jpg'; 
-import AlbumCover_Thumbnail_5 from './albums covers/inner1-5.Individual thought patterns.jpg'; 
-import AlbumCover_5 from './albums covers/5.Individual thought patterns.jpg'; 
-import AlbumCover_6 from './albums covers/6.Symbolic.jpg'; 
-import AlbumCover_7 from './albums covers/7.The sound of perseverance.jpg'; 
-
+import AlbumCover_1 from './albums covers/1.Scream bloody gore.jpg';
+import AlbumCover_2 from './albums covers/2.Leprosy.jpg';
+import AlbumCover_3 from './albums covers/3.Spiritual healing.jpg';
+import AlbumCover_4 from './albums covers/4.Human.jpg';
+import AlbumCover_Thumbnail_5 from './albums covers/inner1-5.Individual thought patterns.jpg';
+import AlbumCover_5 from './albums covers/5.Individual thought patterns.jpg';
+import AlbumCover_6 from './albums covers/6.Symbolic.jpg';
+import AlbumCover_7 from './albums covers/7.The sound of perseverance.jpg';
 
 import { useNavigate } from 'react-router-dom';
 
 const LevelsSlider = () => {
-
   return (
-
-    <div class="slider">
-
-      <div class="field-top"></div>
-
-      <div class="slider-fields">
-
-        
-        <div class="slider-field">
-
-          <div class="slider-field-inner1">
-            <img src={AlbumCover_1} id="inner1-Scream_bloody_gore"/>
+    <div className="slider">
+      <div className="field-top"></div>
+      <div className="slider-fields">
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_1} alt="Scream Bloody Gore album cover" id="inner1-Scream_bloody_gore" />
           </div>
-
-          <div class="slider-field-inner2">
-            <img src={AlbumCover_1} />
-            <div class="slider-field-title">Scream bloody gore</div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_1} alt="Scream Bloody Gore album cover" />
+            <div className="slider-field-title">Scream bloody gore</div>
           </div>
-
         </div>
-
-
-
-        <div class="slider-field">
-
-            <div class="slider-field-inner1">
-              <img src={AlbumCover_2} id="inner1-Leprosy"/>
-                
-            </div>
-
-            <div class="slider-field-inner2">
-              <img src={AlbumCover_2} />
-              <div class="slider-field-title">Leprosy</div>
-            </div>
-
-        </div>
-
-
-        <div class="slider-field">
-
-          <div class="slider-field-inner1">
-            <img src={AlbumCover_3} id="inner1-Spiritual_healing"/>
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_2} alt="Leprosy album cover" id="inner1-Leprosy" />
           </div>
-
-          <div class="slider-field-inner2">
-            <img src={AlbumCover_3} />
-            <div class="slider-field-title">Spiritual healing</div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_2} alt="Leprosy album cover" />
+            <div className="slider-field-title">Leprosy</div>
           </div>
-
         </div>
-
-
-
-        <div class="slider-field">
-            
-            <div class="slider-field-inner1">
-              <img src={AlbumCover_4} id="inner1-Human"/>
-            </div>
-
-            <div class="slider-field-inner2">
-              <img src={AlbumCover_4} />
-                <div class="slider-field-title">Human</div>
-            </div>
-
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_3} alt="Spiritual Healing album cover" id="inner1-Spiritual_healing" />
+          </div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_3} alt="Spiritual Healing album cover" />
+            <div className="slider-field-title">Spiritual healing</div>
+          </div>
         </div>
-
-
-        <div class="slider-field">
-            
-            <div class="slider-field-inner1">
-              <img src={AlbumCover_Thumbnail_5} id="inner1-Individual_thought_patterns"/>
-            </div>
-
-            <div class="slider-field-inner2">
-              <img src={AlbumCover_5} />
-                <div class="slider-field-title">Individual thought patterns</div>
-            </div>
-
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_4} alt="Human album cover" id="inner1-Human" />
+          </div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_4} alt="Human album cover" />
+            <div className="slider-field-title">Human</div>
+          </div>
         </div>
-
-
-        <div class="slider-field">
-            
-            <div class="slider-field-inner1">
-              <img src={AlbumCover_6} id="inner1-Symbolic"/>
-            </div>
-
-            <div class="slider-field-inner2">
-              <img src={AlbumCover_6} />
-                <div class="slider-field-title">Symbolic</div>
-            </div>
-
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_Thumbnail_5} alt="Individual Thought Patterns thumbnail" id="inner1-Individual_thought_patterns" />
+          </div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_5} alt="Individual Thought Patterns album cover" />
+            <div className="slider-field-title">Individual thought patterns</div>
+          </div>
         </div>
-
-
-
-        <div class="slider-field">
-            
-            <div class="slider-field-inner1">
-              <img src={AlbumCover_7} id="inner1-The_sound_of_perseverance"/>
-            </div>
-
-            <div class="slider-field-inner2">
-              <img src={AlbumCover_7} />
-                <div class="slider-field-title">The sound of perseverance</div>
-            </div>
-
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_6} alt="Symbolic album cover" id="inner1-Symbolic" />
+          </div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_6} alt="Symbolic album cover" />
+            <div className="slider-field-title">Symbolic</div>
+          </div>
         </div>
-
-
+        <div className="slider-field">
+          <div className="slider-field-inner1">
+            <img src={AlbumCover_7} alt="The Sound of Perseverance album cover" id="inner1-The_sound_of_perseverance" />
+          </div>
+          <div className="slider-field-inner2">
+            <img src={AlbumCover_7} alt="The Sound of Perseverance album cover" />
+            <div className="slider-field-title">The sound of perseverance</div>
+          </div>
+        </div>
       </div>
-      <div class="field-bottom"></div>
-
-
+      <div className="field-bottom"></div>
     </div>
-
   );
 };
 
