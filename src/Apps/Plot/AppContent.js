ecd11fcc66b867a8fa059b3fad1ea55eb8d6ae70
Me6111 <maksymmpawlowski@gmail.com>
2024-11-05 00:24:42 +0000

    Updated app.js

--- src/Apps/Plot/AppContent.js
+++ src/Apps/Plot/AppContent.js
@@ -1,18 +1,16 @@
-
-
-
 import './AppContent.css';
 
-
 const AppContent = () => {
-
+  const [state, setState] = useState(null);
+  useEffect(() => {
+    // Add any side effects here if needed
+  }, []);
 
   return (
     <div className="AppContent">
       <div className="AppContent_Field"></div>
       <div className="AppContent_Field"></div>
       <div className="AppContent_Field"></div>
-
     </div>
   );
 };
