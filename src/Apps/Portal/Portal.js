ffb7c6d56b12b17a435837df09bed71aba737df9
Me6111 <maksymmpawlowski@gmail.com>
2024-11-05 00:30:54 +0000

    Updated app.js

--- src/Apps/Portal/Portal.js
+++ src/Apps/Portal/Portal.js
@@ -1,16 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Portal.css';
 import MainMenuSet from '../../components/MainMenuSet/MainMenuSet';
 import AppContent from './AppContent';
 
-
-
 const Portal = () => {
-  const ElementToRef = useRef(null); // Create a ref for the appContainer 
+  const [state, setState] = useState(null);
+  const ElementToRef = useRef(null);
 
   useEffect(() => {
-
-
+    // Initialize ElementToRef.current value here if needed
+    ElementToRef.current = null;
   }, []);
 
   return (
