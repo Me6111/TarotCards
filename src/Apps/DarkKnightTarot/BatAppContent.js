d29e45b9dfe655157d8575de885fa23d7bd13392
Me6111 <maksymmpawlowski@gmail.com>
2024-11-22 18:58:58 +0000

    Updated app.js

--- src/Apps/DarkKnightTarot/BatAppContent.js
+++ src/Apps/DarkKnightTarot/BatAppContent.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BatPasswordComponent from './BatPasswordComponent/BatPasswordComponent';
 
 const BatAppContent = () => {
-
-
   return (
     <div className="BatAppContent">
       <BatPasswordComponent />
