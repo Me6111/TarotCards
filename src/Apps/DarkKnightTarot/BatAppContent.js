e2d1a7034f06fc0318b4029a0e6cceb069378b0c
Me6111 <maksymmpawlowski@gmail.com>
2024-11-22 18:53:57 +0000

    Updated app.js

--- src/Apps/DarkKnightTarot/BatAppContent.js
+++ src/Apps/DarkKnightTarot/BatAppContent.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import BatPasswordComponent from './BatPasswordComponent/BatPasswordComponent';
 
 const BatAppContent = () => {
-  const [passwordOk, setPasswordOk] = useState(false);
 
   useEffect(() => {
     if (passwordOk) {
