d19ef305c4ada739a18ba963bd752b6da66b1797
Me6111 <maksymmpawlowski@gmail.com>
2024-11-27 02:58:29 +0000

    Updated app.js

--- src/Apps/DarkKnightTarot/DarkKnightTarotCards/BatSliderField.js
+++ src/Apps/DarkKnightTarot/DarkKnightTarotCards/BatSliderField.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './BatSliderField.css';
 
-const BatSliderField = ({ imgPath, title }) => {
+const BatSliderField = ({ imgPath, number, title }) => {
   return (
     <div className="BatSliderField">
       <div className="BatSliderField1">
         <div className="tarot-card">
           <div className="card-content">
-            <div className="card-image">
-              <img src={imgPath} alt={title} />
+            
+            <div className="card-image-container">
+              <div className="card-number0">
+                <div className="card-number">{number}</div>
+              </div>
+                
+              <div className="card-image">
+                <img src={imgPath} alt={title} />
+              </div>
             </div>
+
             <div className="card-title">
               <div className="card-title-text">
                 {title}
